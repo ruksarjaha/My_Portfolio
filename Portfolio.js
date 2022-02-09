d6bdd8fd5fbe7f953dca9f9a16f668cce9dd1e21
Eshan Mohammad <eshan@Eshans-Air.attlocal.net>
2022-02-09 13:08:43 -0500

    adding files

---
+++ Portfolio.js
@@ -0,0 +1,72 @@
+// links
+const links =document.querySelectorAll('.link');
+
+links.forEach(link => {
+    link.addEventListener('click', () => {
+        links.forEach(ele => ele.classList.remove('active'));
+        link.classList.add('active');
+    })
+})
+
+// Creating dynamic project card
+
+const projectContainer= document.querySelector('.project-container');
+projects.forEach(project => {
+    projectContainer.innerHTML += `
+    <div class="project-card" data-tags="#All Projects, ${project.tags}">
+        <img src="${project.image}" alt="">
+            <div class="card-content">
+                <h1 class="project-name">${project.name}</h1>
+                <span class="tags">${project.tags}</span>
+            </div>
+    </div>
+    `;
+
+}) 
+// filters
+
+const filters = document.querySelectorAll('.filter-btn');
+
+filters.forEach(filterBtn => {
+    filterBtn.addEventListener('click', () => {
+        let id= filterBtn.getAttribute('id');
+        let projectCards = document.querySelectorAll('.project-card');
+        projectCards.forEach( card => {
+            if(card.getAttribute('data-tags').includes(id)){
+                card.classList.remove('hide');
+            } else{
+                card.classList.add('hide');
+            }
+        })
+        filters.forEach(btn => btn.classList.remove('active'));
+        filterBtn.classList.add('active');
+    })
+})
+
+//contact form
+const contactBtn = document.querySelector('.contact-btn');
+const firstName = document.querySelector('.first-name');
+const lastName = document.querySelector('.last-name');
+const email = document.querySelector('.email');
+const msg = document.querySelector('.message');
+
+
+contactBtn.addEventListener('click', () => {
+    if(firstName.value.length && lastName.value.length && email.value.length && 
+        msg.value.length){
+        fetch('/mail', {
+            method: 'post',
+            headers: new Headers({'Content-Type': 'application/json'}),
+            body: JSON.stringify({
+                firstname: firstName.value,
+                lastname: lastName.value,
+                email: email.value,
+                msg: msg.value,
+            })
+        })
+        .then(res => res.json())
+        .then(data => {
+            alert(data);
+        })
+    }
+})
---
+++ Projects.js
@@ -0,0 +1,22 @@
+let projects = [
+    {
+        name: "Travel Project",
+        tags: "#ReactJS",
+        image: "image/Travel-project-img.jpeg",
+    },
+    {
+        name: "Clone AMC",
+        tags: "#HTML, #CSS, #JavaScript",
+        image: "image/AMC Clone.jpeg",
+    },
+    {
+        name: "",
+        tags: "#All",
+        image: "image/coming-soon.jpeg",
+    },
+    {
+        name: "",
+        tags: "#All",
+        image: "image/coming-soon.jpeg",
+    }
+]
